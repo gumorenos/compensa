72013674c6592d001e597b1a66c05345def16e5b
Gus MS <gumorenos@gmail.com>
2026-08-21 00:02:09 -0500

    fix(web): resolve TypeScript ESM imports with webpack

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.resolve.extensionAlias = {
+      ".js": [".ts", ".tsx", ".js"],
+      ".mjs": [".mts", ".mjs"],
+      ".cjs": [".cts", ".cjs"],
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
